cdbd32e723128d8d8f25376d66c4cef7c97922c7
kiaSso <mauro.mancinetti@gmail.com>
2017-04-26 18:45:11 +0100

    - added swagger for API documentation

--- server/meals-on-wheels/src/main/java/com/meals/on/wheels/MealsOnWheelsApplication.java
+++ server/meals-on-wheels/src/main/java/com/meals/on/wheels/MealsOnWheelsApplication.java
@@ -11,14 +11,27 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+
 import java.util.Arrays;
 
+import com.google.common.base.Predicate;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+import static com.google.common.base.Predicates.or;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MealsOnWheelsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MealsOnWheelsApplication.class, args);
 	}
+
 	@Bean
 	public ServletRegistrationBean h2servletRegistration(){
 		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
@@ -53,6 +66,25 @@ public class MealsOnWheelsApplication {
 		config.addAllowedMethod("PUT");
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
+	}
+
+
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+				.apiInfo(apiInfo()).select().paths(postPaths()).build();
+	}
+
+	private Predicate<String> postPaths() {
+		return or(regex("/meals-on-wheels/*"), regex("/meals-on-wheels/*"));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("JavaInUse API")
+				.description("JavaInUse API reference for developers")
+				.termsOfServiceUrl("http://javainuse.com")
+				.contact("javainuse@gmail.com").license("JavaInUse License")
+				.licenseUrl("javainuse@gmail.com").version("1.0").build();
 	}
 
 }
