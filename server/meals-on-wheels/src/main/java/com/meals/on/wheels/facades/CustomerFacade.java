ce07af673a3405848445a55ec6df99aa9bcd5ae1
mauro <mauro.mancinetti@sideffect.co.uk>
2017-04-28 11:54:00 +0100

    reorganised imports

--- server/meals-on-wheels/src/main/java/com/meals/on/wheels/facades/CustomerFacade.java
+++ server/meals-on-wheels/src/main/java/com/meals/on/wheels/facades/CustomerFacade.java
@@ -7,7 +7,6 @@ import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
